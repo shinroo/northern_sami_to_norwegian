1c9dccfb9cf88b7e0f68c0e069505d32cefb141a
Robert Focke <robert.focke@member.fsf.org>
2020-06-16 09:28:07 +0200

    tsv -> ##sv, split lines on ## to get sami word, norwegian word and additional info

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
     let day = new Date().getDay();
-    $('#daily_line').text(daily_list[day % daily_list.length]);
+    let daily_line = daily_list[day % daily_list.length];
+    daily_line = daily_line.split("##");
+    $('#sami_word').text(daily_line[0]);
+    $('#norwegian_word').text(daily_line[1]);
+    $('#additional_info').text(daily_line[2]);
 });
